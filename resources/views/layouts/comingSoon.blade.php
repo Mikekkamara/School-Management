38cabc63b75252cbfc47206e324bd14e6d8c2a4c
Mikekkamara <mikekkamara@gmail.com>
2021-06-03 18:53:27 +0300

    update

--- resources/views/layouts/comingSoon.blade.php
+++ resources/views/layouts/comingSoon.blade.php
@@ -4,6 +4,7 @@
         <p>coming soon <span>!</span></p>
         <p>we are doing our best to make this feature available</p>
         <a href="">learn more</a>
+            {{-- <span class="joke">Ooops! There is nothing more to learn 😂</span> --}}
     </section>
 @endsection
 
